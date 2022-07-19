605e220a6dae647db8c1f780b3f83f8507201028
peanut <1178616351@qq.com>
2022-07-19 18:23:04 +0800

    peanut commit

---
+++ gomoku.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: gomoku.h
+> Author: CarlSun
+> Mail:sunxiuyang04@gmail.com 
+> Created Time: Sat Aug 20 20:28:12 2016
+ ************************************************************************/
+
+#ifndef _5ZIQI_
+#define _5ZIQI_
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define N 10
+typedef struct tag{
+		int x,y,xl,yl;
+}node;
+#endif
